c94b4b917f4953f06b4155518ed398b44611d483
Carlos Villanueva <ceva_19@hotmail.com>
2018-02-05 15:47:06 -0300

    Char Data Type

---
+++ Java_References_10/src/ch03/datatypes/chars/CharDemo.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ch03.datatypes.chars;
+
+/**
+ *
+ * @author barcvilla
+ */
+public class CharDemo {
+    
+    public static void main(String[] args) {
+        char ch1, ch2;
+        ch1 = 88; // codigo para x
+        ch2 = 'Y';
+        
+        System.out.print("ch1 and ch2: ");
+        System.out.println(ch1 + " " + ch2);
+    }
+}
